d0e9bc36d69d0ae0caf89be8fbcdde274ae6c67f
Irian Cortés <icortesdev@proton.me>
2023-11-15 22:00:57 +0100

    contacto rss

--- src/pages/Paellas.tsx
+++ src/pages/Paellas.tsx
@@ -10,7 +10,7 @@ export const Paellas = () => {
             <nav className="bg-white border-gray-200 bg-zinc-900">
                 <h1 className="text-5xl mx-auto text-center mb-10 ">Catering Vicente</h1>
                 <div className='flex items-center justify-center'>
-                    <h3 className='text-2xl font-bold lg:text-2xl text-xl'>Paellas gigantes para todo tipo de eventos</h3>
+                    <h3 className='text-2xl font-bold lg:text-3xl '>Paellas gigantes para todo tipo de eventos</h3>
                 </div>
 
                 <div className="bg-white border-gray-200 bg-zinc-900 max-w-screen-xl py-6 sm:py-8 lg:py-12 items-center justify-between mx-auto p-4">
