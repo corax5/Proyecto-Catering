1485b25a04898e2dcb91fae40ad9d02a5397f484
Irian Cortés <icortesdev@proton.me>
2023-11-30 17:02:49 +0100

    edit catering

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -37,7 +37,7 @@ export const Navbar = () => {
     return (
         <nav className="bg-white border-gray-200 bg-zinc-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4  ">
-                <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <Image
                         src={'/images/Proyecto nuevo (2).png'}
                         alt="Catering Logo"
